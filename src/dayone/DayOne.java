1f11631841a37fbd77536cdb15e627ae9bb7cc86
Dominic Orme <Dominic.Orme@googlemail.com>
2024-12-01 14:09:15 +0000

    Day One

---
+++ src/dayone/DayOne.java
@@ -0,0 +1,60 @@
+package dayone;
+
+import util.FileReader;
+
+import java.util.*;
+
+public class DayOne {
+    public static void main(String[] args) {
+        DayOne.partOne();
+        DayOne.partTwo();
+    }
+
+    public static void partTwo() {
+        Vector<String> lines = FileReader.readFile("./src/dayone/input.txt");
+        List<Integer> first = new Vector<>();
+        Map<Integer, Integer> occurrences = new HashMap<>();
+        lines.forEach(s -> {
+            String[] parts = s.split(" {3}");
+            first.add(Integer.parseInt(parts[0]));
+            int second = Integer.parseInt(parts[1]);
+            Integer value = occurrences.putIfAbsent(second, 1);
+            if (value != null) {
+                occurrences.put(second, value + 1);
+            }
+        });
+
+        int total = 0;
+        for (int value : first) {
+            if (occurrences.containsKey(value)) {
+                total += value * occurrences.get(value);
+            }
+        }
+        System.out.println("Total : " + total); // 21142653
+    }
+
+    public static void partOne() {
+        Vector<String> lines = FileReader.readFile("./src/dayone/input.txt");
+        int total = 0;
+        List<Integer> first = new Vector<>();
+        List<Integer> second = new Vector<>();
+        lines.forEach(s -> {
+            String[] parts = s.split(" {3}");
+            first.add(Integer.parseInt(parts[0]));
+            second.add(Integer.parseInt(parts[1]));
+        });
+        Collections.sort(first);
+        Collections.sort(second);
+
+        if (first.size() != second.size()) {
+            System.out.println("Sizes different");
+            return;
+        }
+
+        for (int i = 0; i < first.size(); i++) {
+            total += Math.abs(second.get(i) - first.get(i));
+        }
+
+        System.out.println("total: " + total); // Answer: 2285373
+    }
+}
---
+++ src/dayone/Readme.md
@@ -0,0 +1,9 @@
+# Part 1:
+
+To find out, pair up the numbers and measure how far apart they are. Pair up the smallest number in the left list with the smallest number in the right list, then the second-smallest left number with the second-smallest right number, and so on.
+
+Within each pair, figure out how far apart the two numbers are.
+
+# Part 2:
+
+figure out exactly how often each number from the left list appears in the right list. Calculate a total similarity score by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
