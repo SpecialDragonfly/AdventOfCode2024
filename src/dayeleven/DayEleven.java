16352484e339edebc7cb160ae307256f1af92ef9
Dominic Orme <Dominic.Orme@googlemail.com>
2024-12-11 21:06:32 +0000

    Day 11 Part 2

--- src/dayeleven/DayEleven.java
+++ src/dayeleven/DayEleven.java
@@ -2,28 +2,81 @@ package dayeleven;
 
 import util.Util;
 
-import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Vector;
+import java.util.*;
 
 public class DayEleven {
     public static void main(String[] args) {
         String line = Util.getFileAsLine("./src/dayeleven/input.txt");
-        List<Stone> stones = Arrays.stream(line.split(" ")).map(value -> new Stone(new BigInteger(value))).toList();
-        System.out.println(stones);
+        List<Stone> stones = Arrays.stream(line.split(" ")).map(value -> new Stone(Long.valueOf(value))).toList();
+        HashMap<Long, Stone> stoneCache = new HashMap<>();
 
-        int blinkTimes = 25;
-        for (int i = 0; i < blinkTimes; i++) {
-            for (Stone stone : stones) {
-                stone.blink();
-            }
-        }
-        Vector<Stone> output = new Vector<>();
+        partOne(stones, 25, stoneCache);
+        partTwo(stones, 75, stoneCache);
+    }
+
+    public static void partTwo(List<Stone> stones, int times, HashMap<Long, Stone> stoneCache) {
+        // Set up initial values
+        HashMap<Long, Long> stoneCount = new HashMap<>();
         for (Stone stone : stones) {
-            output.addAll(stone.getChildren());
+            if (!stoneCount.containsKey(stone.getValue())) {
+                stoneCount.put(stone.getValue(), 0L);
+            }
+            stoneCount.merge(stone.getValue(), 1L, Long::sum);
         }
-        System.out.println(output);
-        System.out.println(output.size());
+
+        // Foreach blink
+        for (int i = 0; i < times; i++) {
+            HashMap<Long, Long> newStoneCount = new HashMap<>();
+            // Iterate through unique stonecount values
+            for (Map.Entry<Long, Long> entry: stoneCount.entrySet()) {
+                Long stoneValue = entry.getKey();
+                if (!stoneCache.containsKey(stoneValue)) {
+                    Stone stone = new Stone(stoneValue);
+                    stone.blink();
+                    stoneCache.put(stoneValue, stone);
+                }
+                Vector<Stone> children = stoneCache.get(stoneValue).getChildren();
+                for (Stone child : children) {
+                    newStoneCount.merge(child.getValue(), entry.getValue(), Long::sum);
+                }
+            }
+            // These are the new stones, and their associated quantities.
+            stoneCount = newStoneCount;
+        }
+
+        System.out.println("Total: " + stoneCount.values().stream().reduce(0L, Long::sum));
+    }
+
+    public static void partOne(List<Stone> stones, int times, HashMap<Long, Stone> stoneCache) {
+        // Set up initial values
+        HashMap<Long, Long> stoneCount = new HashMap<>();
+        for (Stone stone : stones) {
+            if (!stoneCount.containsKey(stone.getValue())) {
+                stoneCount.put(stone.getValue(), 0L);
+            }
+            stoneCount.merge(stone.getValue(), 1L, Long::sum);
+        }
+
+        // Foreach blink
+        for (int i = 0; i < times; i++) {
+            HashMap<Long, Long> newStoneCount = new HashMap<>();
+            // Iterate through unique stonecount values
+            for (Map.Entry<Long, Long> entry: stoneCount.entrySet()) {
+                Long stoneValue = entry.getKey();
+                if (!stoneCache.containsKey(stoneValue)) {
+                    Stone stone = new Stone(stoneValue);
+                    stone.blink();
+                    stoneCache.put(stoneValue, stone);
+                }
+                Vector<Stone> children = stoneCache.get(stoneValue).getChildren();
+                for (Stone child : children) {
+                    newStoneCount.merge(child.getValue(), 1L, Long::sum);
+                }
+            }
+            // These are the new stones, and their associated quantities.
+            stoneCount = newStoneCount;
+        }
+
+        System.out.println("Total: " + stoneCount.values().stream().reduce(0L, Long::sum));
     }
 }
--- src/dayeleven/Stone.java
+++ src/dayeleven/Stone.java
@@ -1,68 +1,45 @@
 package dayeleven;
 
-import java.math.BigInteger;
 import java.util.Vector;
 
 public class Stone {
-    private BigInteger value;
+    private final Long value;
     private final Vector<Stone> children;
 
-    public Stone(BigInteger value) {
+    public Stone(Long value) {
         this.value = value;
         this.children = new Vector<>();
     }
 
     public void blink() {
-        if (children.isEmpty()) {
         /*
 If the stone is engraved with the number 0, it is replaced by a stone engraved with the number 1.
 If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
 If none of the other rules apply, the stone is replaced by a new stone; the old stone's number multiplied by 2024 is engraved on the new stone.
          */
-            if (this.value.equals(new BigInteger("0"))) {
-                this.children.add(new Stone(new BigInteger("1")));
-            } else if (String.valueOf(this.value).length() % 2 == 0) {
-                String x = String.valueOf(this.value);
-                String x0 = x.substring(0, x.length()/2);
-                String x1 = x.substring(x.length()/2);
-                this.children.add(new Stone(new BigInteger(x0)));
-                this.children.add(new Stone(new BigInteger(x1)));
-            } else {
-                this.children.add(new Stone(this.value.multiply(new BigInteger("2024"))));
-            }
+
+        if (this.value.equals(0L)) {
+            children.add(new Stone(1L));
+        } else if (String.valueOf(this.value).length() % 2 == 0) {
+            String x = String.valueOf(this.value);
+            String x0 = x.substring(0, x.length()/2);
+            String x1 = x.substring(x.length()/2);
+            children.add(new Stone(Long.parseLong(x0)));
+            children.add(new Stone(Long.parseLong(x1)));
         } else {
-            for (Stone stone : children) {
-                stone.blink();
-            }
+            children.add(new Stone(this.value * 2024L));
         }
+    }
+
+    public Vector<Stone> getChildren() {
+        return this.children;
     }
 
     public String toString() {
         return this.value + "";
     }
 
-    public Vector<Stone> getChildren() {
-        if (this.children.isEmpty()) {
-            Vector<Stone> x = new Vector<>();
-            x.add(this);
-            return x;
-        }
-        Vector<Stone> x = new Vector<>();
-        for (Stone stone : this.children) {
-            x.addAll(stone.getChildren());
-        }
-        return x;
-    }
-
-    public int getChildCount() {
-        int total = 0;
-        if (this.children.isEmpty()) {
-            total++; // this one.
-        } else {
-            for (Stone stone : this.children) {
-                total += stone.getChildCount();
-            }
-        }
-        return total;
+    public Long getValue() {
+        return this.value;
     }
 }
